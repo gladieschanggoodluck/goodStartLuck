f08c1fb40e9d35d7e08dee71a647cd340fe62d60
gladieschanggoodluck <gladiesgoodluck@gmail.com>
2019-06-08 19:01:49 +0000

    Add a good message describing what you actually changed since your last commit in here

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -3,7 +3,18 @@ class GramsController < ApplicationController
     @gram = Gram.new
   end
 
-
   def index
   end
+
+  def create
+    @gram = Gram.create(gram_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def gram_params
+    params.require(:gram).permit(:message)
+  end
+
 end
