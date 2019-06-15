a6a40c0ff108729a83ba639bbf4e4426fc618f39
gladieschanggoodluck <gladiesgoodluck@gmail.com>
2019-06-15 02:30:13 +0000

    Add a good message describing what you actually changed since your last commit in here

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,9 @@
-factory :user do
+FactoryBot.define do
+    factory :user do
         sequence :email do |n|
             "dummyemail#{n}@gmail.com"
         end
         password "secretPassword"
         password_confirmation "secretPassword"
+    end
 end
