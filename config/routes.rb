6234ef2eaa86571b98530e079dc4b2c0df098e44
gladieschanggoodluck <gladiesgoodluck@gmail.com>
2019-06-16 01:58:50 +0000

    Add a good message describing what you actually changed since your last commit in here

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   resources :grams, only: [:new, :create, :show]
   resources :grams, only: [:new, :create, :show, :edit]
   resources :grams, only: [:new, :create, :show, :edit, :update]
+  resources :grams, only: [:new, :create, :show, :edit, :update, :destroy]
+  resources :grams
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
